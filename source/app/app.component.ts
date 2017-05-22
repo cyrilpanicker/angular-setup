ecd0fde6196eb5d322e601e867d6157fe2133663
cyrilpanicker <cyrilpanicker@gmail.com>
2017-05-22 17:53:57 +0530

    added changes

--- source/app/app.component.ts
+++ source/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'app',
-    templateUrl:'./app.component.html'
+    templateUrl:'./app.component.html',
+    // styleUrls:['./app.component.css']
+    styles:[`
+        .user-input, .user-ouput{
+            margin-top: 30px;
+        }
+    `]
 })
 export class AppComponent{
     text='Hello World..';
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     devtool:'cheap-module-eval-source-map',
     entry:{
+        'styles':'./source/styles',
         'polyfills':'./source/polyfills',
         'libs':'./source/libs',
         'app':'./source/main'
     },
     output:{
-        path:__dirname+'/build',
+        path:__dirname+'/build/bundles',
         // publicPath:'/',
         filename:'[name].js',
         chunkFilename:'chunk[id].js'
@@ -21,20 +22,32 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name:['app','libs','polyfills']
         }),
-        new HtmlWebpackPlugin({
-            title:'Angular Setup',
-            template:'./source/assets/index.html'
-        })
+        new ExtractTextPlugin('[name].css')
     ],
     module:{
         loaders:[
             {
                 test:/\.ts$/,
-                loaders:['awesome-typescript-loader', 'angular2-template-loader', 'angular2-router-loader']
+                loaders:['awesome-typescript-loader', 'angular2-template-loader','angular2-router-loader']
             },
             {
                 test:/\.html$/,
                 loader:'html-loader'
+            },
+            // {
+            //     test:/\.css$/,
+            //     loader:'style-loader!css-loader'
+            // },
+            {
+                test:/\.css$/,
+                loader:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:'css-loader'
+                })
+            },
+            {
+                test:/\.(eot|svg|ttf|woff|woff2)$/,
+                loader:'file-loader?name=fonts/[name].[ext]'
             }
         ]
     }
